c245df93fe048e9edee25b8005d7467140d83577
stizianel <stizianel@gmail.com>
2014-02-14 15:18:39 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Minou::Application.config.secret_key_base = '493ecacf0bd1fdc4e7bcb432f46d1f2cef6a0c779d7d73288aca389003318edd01a369a5097e6e6268e9920dd5884c189052ce9ed602cd6be336a57933458d1c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Minou::Application.config.session_store :cookie_store, key: '_minou_session'
