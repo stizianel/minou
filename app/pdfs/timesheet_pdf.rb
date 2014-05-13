f11821e906bb5077836f18c9f07949596c1494d5
stizianel <stizianel@gmail.com>
2014-05-13 11:38:57 +0200

    al 1305

--- app/pdfs/timesheet_pdf.rb
+++ app/pdfs/timesheet_pdf.rb
@@ -5,10 +5,17 @@ class TimesheetPdf < Prawn::Document
   	@timesheet = timesheet
   	timesheet_header
   	timesheet_body
+    timesheet_summary_header
+    timesheet_summary
   end
 
   def timesheet_header
   	text "Mese di #{@timesheet.mese} #{@timesheet.year} #{@timesheet.user.name}", size: 22, style: :bold
+  end
+
+  def timesheet_summary_header
+    move_down 20
+    text "Riepilogo ore per causale", size: 12, style: :bold
   end
 
   def timesheet_body
@@ -33,16 +40,17 @@ class TimesheetPdf < Prawn::Document
 
   def timesheet_summary
     move_down 20
-    font_size 12
-    table timesheet_summary_rows do
+    font_size 8
+    table timesheet_summary_rows, :cell_style => { :overflow => :shrink_to_fit, :min_font_size => 6} do
       row(0).font_style = :bold
       self.header = true
     end
   
   end
   def timesheet_summary_rows
-    [["nome", "anno", "mese", "voce", "ore"]] +
-    
-
+    [["causale", "ore"]] +
+    Timesheet.totali(@timesheet.id).map do |item|
+      [item[3], item[4]]
+    end
   end
 end
